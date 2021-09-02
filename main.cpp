06a247b66c0322a3b51e525ccf744d8138ebf704
10RRK <shkab@163.com>
2021-09-02 17:10:50 +0800

    first

--- main.cpp
+++ main.cpp
@@ -15,20 +15,22 @@
 #define MAX_EVENT_NUMBER 10000  // 监听的最大的事件数量
 
 // 添加文件描述符
-extern void addfd( int epollfd, int fd, bool one_shot );
-extern void removefd( int epollfd, int fd );
+extern void addfd(int epollfd, int fd, bool one_shot);
+extern void removefd(int epollfd, int fd);
 
-void addsig(int sig, void( handler )(int)){
+void addsig(int sig, void(handler)(int))
+{
     struct sigaction sa;
-    memset( &sa, '\0', sizeof( sa ) );
+    memset(&sa, '\0', sizeof(sa));
     sa.sa_handler = handler;
-    sigfillset( &sa.sa_mask );
-    assert( sigaction( sig, &sa, NULL ) != -1 );
+    sigfillset(&sa.sa_mask);
+    assert(sigaction(sig, &sa, NULL) != -1);
 }
 
-int main( int argc, char* argv[] ) {
-    
-    if( argc <= 1 ) {
+int main(int argc, char* argv[]) 
+{
+    if( argc <= 1 ) 
+    {
         printf( "usage: %s port_number\n", basename(argv[0]));
         return 1;
     }
@@ -36,16 +38,16 @@ int main( int argc, char* argv[] ) {
     int port = atoi( argv[1] );
     addsig( SIGPIPE, SIG_IGN );
 
-    threadPool< http_conn >* pool = NULL;
+    threadPool<http_conn>* pool = NULL;
     try {
         pool = new threadPool<http_conn>;
     } catch( ... ) {
         return 1;
     }
 
-    http_conn* users = new http_conn[ MAX_FD ];
+    http_conn* users = new http_conn[MAX_FD];
 
-    int listenfd = socket( PF_INET, SOCK_STREAM, 0 );
+    int listenfd = socket(PF_INET, SOCK_STREAM, 0);
 
     int ret = 0;
     struct sockaddr_in address;
@@ -55,71 +57,71 @@ int main( int argc, char* argv[] ) {
 
     // 端口复用
     int reuse = 1;
-    setsockopt( listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof( reuse ) );
-    ret = bind( listenfd, ( struct sockaddr* )&address, sizeof( address ) );
-    ret = listen( listenfd, 5 );
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse));
+    ret = bind(listenfd, (struct sockaddr*)&address, sizeof(address));
+    ret = listen(listenfd, 5);
 
     // 创建epoll对象，和事件数组，添加
-    epoll_event events[ MAX_EVENT_NUMBER ];
-    int epollfd = epoll_create( 5 );
+    epoll_event events[MAX_EVENT_NUMBER];
+    int epollfd = epoll_create(5);
     // 添加到epoll对象中
-    addfd( epollfd, listenfd, false );
+    addfd(epollfd, listenfd, false);
     http_conn::m_epollfd = epollfd;
 
-    while(true) {
+    while(true) 
+    {
+        int number = epoll_wait(epollfd, events, MAX_EVENT_NUMBER, -1);
         
-        int number = epoll_wait( epollfd, events, MAX_EVENT_NUMBER, -1 );
-        
-        if ( ( number < 0 ) && ( errno != EINTR ) ) {
-            printf( "epoll failure\n" );
+        if((number < 0) && (errno != EINTR)) 
+        {
+            printf("epoll failure\n");
             break;
         }
 
-        for ( int i = 0; i < number; i++ ) {
-            
+        for(int i = 0; i < number; i++) 
+        {
             int sockfd = events[i].data.fd;
             
-            if( sockfd == listenfd ) {
-                
+            if(sockfd == listenfd) 
+            {
                 struct sockaddr_in client_address;
                 socklen_t client_addrlength = sizeof( client_address );
                 int connfd = accept( listenfd, ( struct sockaddr* )&client_address, &client_addrlength );
                 
-                if ( connfd < 0 ) {
-                    printf( "errno is: %d\n", errno );
+                if (connfd < 0) 
+                {
+                    printf("errno is: %d\n", errno);
                     continue;
                 } 
 
-                if( http_conn::m_user_count >= MAX_FD ) {
+                if(http_conn::m_user_count >= MAX_FD) 
+                {
                     close(connfd);
                     continue;
                 }
-                users[connfd].init( connfd, client_address);
-
-            } else if( events[i].events & ( EPOLLRDHUP | EPOLLHUP | EPOLLERR ) ) {
-
+                users[connfd].init(connfd, client_address);
+            } 
+            else if(events[i].events & (EPOLLRDHUP | EPOLLHUP | EPOLLERR)) 
+            {
                 users[sockfd].close_conn();
-
-            } else if(events[i].events & EPOLLIN) {
-
-                if(users[sockfd].read()) {
+            } 
+            else if(events[i].events & EPOLLIN) 
+            {
+                if(users[sockfd].read()) 
                     pool->addTask(users + sockfd);
-                } else {
+                else
                     users[sockfd].close_conn();
-                }
-
-            }  else if( events[i].events & EPOLLOUT ) {
-
-                if( !users[sockfd].write() ) {
+            }  
+            else if(events[i].events & EPOLLOUT) 
+            {
+                if(!users[sockfd].write()) 
                     users[sockfd].close_conn();
-                }
-
             }
         }
     }
     
-    close( epollfd );
-    close( listenfd );
+    close(epollfd);
+    close(listenfd);
     delete [] users;
     delete pool;
     return 0;
--- threadpool.h
+++ threadpool.h
@@ -8,7 +8,6 @@
 #include <vector>
 #include <exception>
 #include <functional>
-// #include <pthread.h>
 #include <iostream>
 
 // 线程池类，将它定义为模板类是为了代码复用，模板参数T是任务类
@@ -25,9 +24,11 @@ private:
     void threadFunc();
 private:
     int m_threadNum;  // 工作线程的数量
-    std::vector<std::shared_ptr<std::thread>> m_threads; // pthread_t * m_threads;  // 描述线程池的数组，大小为m_threadNum 
+    // std::vector<std::shared_ptr<std::thread>> m_threads;  // 描述线程池的数组，大小为m_threadNum 
+    std::vector<std::thread*> m_threads;
     int m_max_requests;     // 任务队列中最多允许的、等待处理的请求的数量
-    std::list<std::shared_ptr<Task>> m_taskList; // std::list<Task*> m_taskList;   // 任务队列
+    // std::list<std::shared_ptr<Task>> m_taskList;   // 任务队列
+    std::list<Task*> m_taskList;
     std::mutex m_mutex;    // 保护任务队列和条件变量的互斥锁 
     std::condition_variable m_cv;   // 是否有任务需要处理   
     bool m_stop;    // 是否结束线程                  
@@ -45,12 +46,15 @@ threadPool<Task>::threadPool(int threadNum, int max_requests) :
     for(int i = 0; i < threadNum; ++i)
     {
         std::cout << "create the " << i << "th thread" << std::endl;
-        std::shared_ptr<std::thread> spThread;
-        spThread.reset(new std::thread(std::bind(&threadPool::threadFunc, this)));
-        if(!spThread)
-            throw std::exception();
-        m_threads.push_back(spThread);
-        spThread->detach();
+        // std::shared_ptr<std::thread> spThread;
+        // spThread.reset(new std::thread(std::bind(&threadPool::threadFunc, this)));
+        // if(!spThread)
+        //     throw std::exception();
+        // m_threads.push_back(spThread);
+        // spThread->detach();
+        std::thread* thread = new std::thread(std::bind(&threadPool::threadFunc, this));
+        m_threads.push_back(thread);
+        thread->detach();
     }
 }
 
@@ -60,8 +64,13 @@ threadPool<Task>::~threadPool()
     m_stop = true;
     {
         std::unique_lock<std::mutex> guard(m_mutex);
-        for(auto& iter : m_taskList)
-            iter.reset();
+        // for(auto& iter : m_taskList)
+        //     iter.reset();
+        while(!m_threads.empty())
+        {
+            std::thread* thread = m_threads.back();
+            m_threads.pop_back();
+        }
         m_taskList.clear();
     }
 }
@@ -69,23 +78,25 @@ threadPool<Task>::~threadPool()
 template <typename Task>
 bool threadPool<Task>::addTask(Task* task)
 {
-    std::shared_ptr<Task> spTask;
-    spTask.reset(task);
+    // std::shared_ptr<Task> spTask;
+    // spTask.reset(task);
     // 操作工作队列时一定要加锁，因为它被所有线程共享。
     {
         std::lock_guard<std::mutex> guard(m_mutex);   
         if (m_taskList.size() > m_max_requests) 
             return false;
-        m_taskList.push_back(spTask);
+        m_taskList.push_back(task);
     }
     m_cv.notify_one();
+    std::cout << "add" << std::endl;
     return true;
 }
 
 template <typename Task>
 void threadPool<Task>::threadFunc()
 {
-    std::shared_ptr<Task> spTask;
+    // std::shared_ptr<Task> spTask;
+    Task* task = nullptr;
     while(1) 
     {
         {
@@ -98,13 +109,16 @@ void threadPool<Task>::threadFunc()
             }
             if(m_stop)
                 break;
-            spTask = m_taskList.front();
+            task = m_taskList.front();
+            // spTask = m_taskList.front();
             m_taskList.pop_front();
         }
-        if (spTask == NULL) 
-            continue;
-        spTask->process();
+        // if (spTask == NULL) 
+        //     continue;
+        // spTask->process();
+        // std::cout << spTask.use_count() << std::endl;
         // spTask.reset();
+        task->process();
     }
 }
 
